4d95c01a0d551e7829aab6ecf9e974d76d0d764a
borislav nikolov <jack@brokensociety.org>
2012-10-07 15:06:14 +0300

    use carbon key events

--- butter/AppDelegate.m
+++ butter/AppDelegate.m
@@ -1,22 +1,23 @@
 #import "AppDelegate.h"
 #import <AppKit/NSAccessibility.h>
+#import <Carbon/Carbon.h>
 
 @implementation AppDelegate
 @synthesize item,menu;
-
 #define LEFT 123
 #define RIGHT 124
 #define DOWN 125
 #define UP 126
-- (void) move:(NSRect)to {
-    AXUIElementRef axu;
+#define FULL 3
+static NSRect up,down,left,right,full;
+static AXUIElementRef _axui;
+void move(NSRect to) {
     AXUIElementRef app;
     CFTypeRef win;
-    axu = AXUIElementCreateSystemWide();
     CFTypeRef _position = (CFTypeRef)(AXValueCreate(kAXValueCGPointType, (const void *)&to.origin));
     CFTypeRef _size = (CFTypeRef)(AXValueCreate(kAXValueCGSizeType, (const void *)&to.size));
     
-    AXUIElementCopyAttributeValue(axu,(CFStringRef)kAXFocusedApplicationAttribute,(CFTypeRef*)&app);
+    AXUIElementCopyAttributeValue(_axui,(CFStringRef)kAXFocusedApplicationAttribute,(CFTypeRef*)&app);
     if(AXUIElementCopyAttributeValue(app,(CFStringRef)NSAccessibilityFocusedWindowAttribute,&win) == kAXErrorSuccess) {
         if(CFGetTypeID(win) == AXUIElementGetTypeID()) {
             AXUIElementSetAttributeValue(win, (CFStringRef)NSAccessibilityPositionAttribute,_position);
@@ -28,10 +29,50 @@
     [NSApp terminate:nil];
 }
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    unsigned int mask = (NSCommandKeyMask|NSAlternateKeyMask);
-    NSRect full = [[NSScreen mainScreen] frame];
-    NSRect up,down,left,right;
+OSStatus key_down_event(EventHandlerCallRef nextHandler,EventRef event,void *unused)
+{
+    EventHotKeyID key;
+    GetEventParameter(event, kEventParamDirectObject, typeEventHotKeyID, NULL, sizeof(key), NULL, &key);
+    switch (key.id) {
+        case LEFT:
+            move(left);
+            break;
+        case RIGHT:
+            move(right);
+            break;
+        case UP:
+            move(up);
+            break;
+        case DOWN:
+            move(down);
+            break;
+        case FULL:
+            move(full);
+            break;
+    }
+    return noErr;
+}
+
+-(void)registerHotKeys
+{
+    EventHotKeyRef keyref;
+    EventHotKeyID kid;
+    EventTypeSpec eventType;
+    eventType.eventClass=kEventClassKeyboard;
+    eventType.eventKind=kEventHotKeyPressed;
+ 	
+    InstallApplicationEventHandler(&key_down_event, 1, &eventType, (void *)CFBridgingRetain(self), NULL);
+ 	int keys[] = {LEFT,RIGHT,UP,DOWN,FULL};
+    for (int i = 0; i < sizeof(keys)/sizeof(keys[0]); i++) {
+        kid.signature = 'htk1';
+        kid.id=keys[i];
+        RegisterEventHotKey(keys[i], cmdKey+optionKey, kid, GetApplicationEventTarget(), 0, &keyref);
+    }
+}
+
+- (void)applicationWillFinishLaunching:(NSNotification *)aNotification {
+    _axui = AXUIElementCreateSystemWide();
+    full = [[NSScreen mainScreen] frame];
     up = down = left = right = full;
     up.size.height /= 2;
     down.size.height /= 2;
@@ -40,30 +81,13 @@
     left.size.width /= 2;
     right.size.width /= 2;
     right.origin.x = right.size.width;
+    
     menu = [[NSMenu alloc] init];
     item = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     [menu addItemWithTitle:@"quit" action:@selector(quit:) keyEquivalent:@""];
     [item setMenu:menu];
     [item setImage:[NSImage imageNamed:@"status-icon"]];
-    
-    [NSEvent addGlobalMonitorForEventsMatchingMask:NSKeyDownMask handler:^(NSEvent *event){
-        if (([event modifierFlags] & mask) == mask) {
-            switch ([event keyCode]) {
-                case LEFT:
-                    [self move:left];
-                    break;
-                case RIGHT:
-                    [self move:right];
-                    break;
-                case UP:
-                    [self move:up];
-                    break;
-                case DOWN:
-                    [self move:down];
-                    break;
-            }
-        }
-    }];
+    [self registerHotKeys];
 }
 
 @end
